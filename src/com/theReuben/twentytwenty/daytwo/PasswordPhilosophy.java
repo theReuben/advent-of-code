85405003c481e1a1fc58aa8c873dcdd34cb49dcb
Reuben <reuben@jerome.home>
2020-12-02 18:14:21 +0000

    added solution for day two part one

---
+++ src/com/theReuben/twentytwenty/daytwo/PasswordPhilosophy.java
@@ -0,0 +1,76 @@
+package com.theReuben.twentytwenty.daytwo;
+
+import com.theReuben.twentytwenty.dayone.ReportRepair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PasswordPhilosophy {
+
+    private final static Logger LOG = LoggerFactory.getLogger(PasswordPhilosophy.class);
+
+    public static void main(String[] args) {
+
+        List<Triple<Pair<Integer, Integer>, String, String>> inputList = readInTxt("src/com/theReuben/twentytwenty/daytwo/PasswordPhilosophy.txt");
+
+        long numberValid = countValidPassword(inputList);
+
+        LOG.info("Number of valid passwords : {}", numberValid);
+
+    }
+
+    private static long countChars(String str, char c) {
+        return str.chars().filter(ch -> ch == c).count();
+    }
+
+    private static boolean isValidPassword(Triple<Pair<Integer, Integer>, String, String> triple) {
+        long count = countChars(triple.getRight(), triple.getMiddle().charAt(0));
+
+        Integer lb = triple.getLeft().getLeft();
+        Integer up = triple.getLeft().getRight();
+
+        return count >= lb && count <= up;
+    }
+
+    private static long countValidPassword(List<Triple<Pair<Integer, Integer>, String, String>> input) {
+        return input.stream().mapToLong(in -> (isValidPassword(in) ? 1 : 0)).sum();
+    }
+
+    // Util
+    private static List<Triple<Pair<Integer, Integer>, String, String>> readInTxt(String path) {
+        List<Triple<Pair<Integer, Integer>, String, String>> result = new ArrayList<>();
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] dataArray = data.split(" ");
+                // Extract Range
+                String[] dataArrayRange = dataArray[0].split("-");
+                Pair<Integer, Integer> pair = Pair.of(Integer.valueOf(dataArrayRange[0]), Integer.valueOf(dataArrayRange[1]));
+
+                // Extract char
+                String character = dataArray[1].split(":")[0];
+
+                // Extract password
+                String password = dataArray[2];
+
+                result.add(Triple.of(pair, character, password));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
